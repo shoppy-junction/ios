cc05a31a831fda5d45c9cc3c79b922ab07cf0fca
Jack Cook <hello@jackcook.nyc>
2018-11-23 19:01:03 +0200

    Initial commit

---
+++ Shoppy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shoppy
+//
+//  Created by Jack Cook on 11/23/18.
+//  Copyright © 2018 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
