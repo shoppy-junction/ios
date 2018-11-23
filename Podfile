06d1ad6a088ca20565ff39d0fd7ae57aaff7d161
Jack Cook <hello@jackcook.nyc>
2018-11-23 20:18:32 +0200

    Added ARCL from source

--- Podfile
+++ Podfile
@@ -4,5 +4,4 @@ target 'Shoppy' do
   use_frameworks!
 
   # Pods for Shoppy
-  pod 'ARCL', '~> 1.0'
 end
--- Shoppy/ViewController.swift
+++ Shoppy/ViewController.swift
@@ -6,15 +6,60 @@
 //  Copyright © 2018 Jack Cook. All rights reserved.
 //
 
-import UIKit
+import CoreLocation
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, SceneLocationViewDelegate {
+    
+    let locationManager = CLLocationManager()
+    var sceneView: SceneLocationView!
+    
+    var currentLocationFound = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView = SceneLocationView()
+        sceneView.locationDelegate = self
+        sceneView.run()
+        
+        view.addSubview(sceneView)
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneView.frame = view.bounds
+    }
+    
+    // MARK: - SceneLocationViewDelegate
+    
+    func sceneLocationViewDidSetupSceneNode(sceneLocationView: SceneLocationView, sceneNode: SCNNode) {
+        let coordinate = CLLocationCoordinate2D(latitude: 60.18505832049373, longitude: 24.831723738743346)
+        let location = CLLocation(coordinate: coordinate, altitude: 30)
+        let image = UIImage(named: "Pin")!
+        
+        let locationNode = LocationAnnotationNode(location: location, image: image)
+        sceneView.addLocationNodeForCurrentPosition(locationNode: locationNode)
+    }
+    
+    func sceneLocationViewDidAddSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        
+    }
+    
+    func sceneLocationViewDidRemoveSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        
+    }
+    
+    func sceneLocationViewDidConfirmLocationOfNode(sceneLocationView: SceneLocationView, node: LocationNode) {
+        
+    }
+    
+    func sceneLocationViewDidUpdateLocationAndScaleOfLocationNode(sceneLocationView: SceneLocationView, locationNode: LocationNode) {
+        
+    }
 }
-
