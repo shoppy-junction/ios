2b9c0c30831508df269188c31e61aaf35c339192
Jack Cook <hello@jackcook.nyc>
2018-11-24 15:39:13 +0200

    Request data from Hyunji's laptop

--- Shoppy/Product.swift
+++ Shoppy/Product.swift
@@ -11,40 +11,22 @@ import SwiftyJSON
 struct Product: Hashable {
     
     let imageURL: URL?
-    let modelName: String
     let name: String
     let price: String
     let pricePerWeight: String
     let weight: String
     
     init?(_ json: JSON) {
-        guard let result = json["results"].array?.first else {
-            return nil
-        }
-        
-        imageURL = URL(string: result["pictureUrls"].array?.first?.dictionary?["original"]?.string ?? "")
-        modelName = ""
-        name = result["marketingName"].dictionary?["finnish"]?.string ?? ""
-        price = "€3.24"
+        imageURL = URL(string: json["pictureUrls"].array?.first?.dictionary?["original"]?.string ?? "")
+        name = json["name"].string ?? ""
+        price = "€" + (json["price"].string ?? "")
         pricePerWeight = "€4.25/kg"
         weight = "300g"
     }
     
     static func requestProductWithEAN(_ ean: String, completion: @escaping (JSON) -> Void) {
-        let url = URL(string: "https://kesko.azure-api.net/v1/search/products")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        
-        let body = [
-            "filters": [
-                "ean": ean
-            ]
-        ]
-        
-        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("80a29c2c6af54807a3b1c57b6c78e032", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
+        let url = URL(string: "http://130.233.86.149:5000/products/\(ean)")!
+        let request = URLRequest(url: url)
         
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: request) { (data, response, error) in
